9a8e674798efcaa08f529bc3b3a4e0be6a54d236
Raymond Hernandez <67213974+rayherna18@users.noreply.github.com>
2026-05-20 17:47:50 -0400

    game logic added and prior functions logic updated

--- index.js
+++ index.js
@@ -1,64 +1,78 @@
 /*
 This will be a console based game, no GUI
 1. Welcome message + call to action for user
-2. Calculate opponent guess ahead of time (random).
+2. Calculate opponent (random).
 3. Prompt user to enter their action either rock/paper/scissors or 1/2/3.
 4. Compare opponents choice with players to see which is winning (rock > scissors, paper > rock, scissors > paper)
 5. Keep tally of wins between player and opponent.
 6. Repeat for best of 3 or other amount.
 7. Once winner is determined, prompt user if they'd like to play again
-8. 
 */
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice() {
+  const CHOICES = ["rock", "paper", "scissors"];
 
-    const CHOICES = ["rock", "paper", "scissors"];
-
-    let index = Math.floor(Math.random() * 3);
-    return CHOICES[index];
+  let index = Math.floor(Math.random() * 3);
+  return CHOICES[index];
 }
 
 function getHumanChoice() {
-    let choice = prompt("What will you throw?");
-    choice = choice.toLowerCase();
-    choice =  choice.trim();
+  let choice = prompt("What will you throw?");
+  choice = choice.toLowerCase();
+  choice = choice.trim();
 
-    if(choice != "rock" && choice != "paper" && choice != "scissors"){
-        alert("Invalid answer. Try again!");
-        getHumanChoice();
-    }
+  if (choice != "rock" && choice != "paper" && choice != "scissors") {
+    alert("Invalid answer. Try again!");
+    return getHumanChoice();
+  }
 
-    return choice;
+  return choice;
 }
 
 function playRound(humanChoice, computerChoice) {
+  let winMsg = `You win! ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} beats ${computerChoice[0].toUpperCase()}${computerChoice.substring(1)}.`;
+  let loseMsg = `You lose! ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} loses to ${computerChoice[0].toUpperCase()}${computerChoice.substring(1)}.`;
+  let tieMsg = `You tied! ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} is equal to ${computerChoice[0].toUpperCase()}${computerChoice.substring(1)}.`;
 
-    let winMsg = `You win! ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} beats ${computerChoice[0].toUpperCase()}${computerChoice.substring(1)}.`;
-    let loseMsg = `You lose! ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} loses to ${computerChoice[0].toUpperCase()}${computerChoice.substring(1)}.`;
-    let tieMsg = `You tied! ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} is equal to ${computerChoice[0].toUpperCase()}${computerChoice.substring(1)}.`; 
-    if (humanChoice === computerChoice) {
-        console.log(tieMsg);
-    }
-    else if (humanChoice === "rock" && computerChoice === "scissors"){
-        humanChoice++;
-        console.log(winMsg);
-    }
-    else if (humanChoice === "paper" && computerChoice === "rock"){
-        humanChoice++;
-        console.log(winMsg);
-    }
-
-    else if (humanChoice === "scissors" && computerChoice === "paper"){
-        humanChoice++;
-        console.log(winMsg);
-    }
-    else {
-        computerChoice++;
-        console.log(loseMsg);
-    }
+  if (humanChoice === computerChoice) {
+    console.log(tieMsg);
+    return "tie";
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log(winMsg);
+    return "win";
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log(winMsg);
+    return "win";
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log(winMsg);
+    return "win";
+  } else {
+    console.log(loseMsg);
+    return "lose";
+  }
 }
 
-playRound(getHumanChoice(), getComputerChoice());
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
+  console.log("Welcome to Rock Paper Scissors. Best of 5 wins.");
+
+  while (humanScore < 3 && computerScore < 3) {
+    let playerWon = playRound(getHumanChoice(), getComputerChoice());
+    if (playerWon === "lose") {
+      computerScore++;
+    } else if (playerWon === "win") {
+      humanScore++;
+    }
+  }
+
+  let finalMsg =
+    humanScore > computerScore
+      ? `Congratulations you won the game!`
+      : `You lost the game. Better luck next time!`;
+
+  console.log(finalMsg);
+}
+
+playGame();
